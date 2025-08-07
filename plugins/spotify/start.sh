a3517220c404eecd5a0c008b52fb1ffce87f13d8
dinpin <dinpin@gmail.com>
2025-08-07 20:49:34 +0200

    d

--- plugins/spotify/start.sh
+++ plugins/spotify/start.sh
@@ -1,39 +1,35 @@
 #!/usr/bin/env bash
+set -e
 
 if [[ -n "$SOUND_DISABLE_SPOTIFY" ]]; then
   echo "Spotify is disabled, exiting..."
   exit 0
 fi
 
-# --- ENV VARS ---
-# SOUND_DEVICE_NAME: Set the device broadcast name for Spotify
-# SOUND_SPOTIFY_BITRATE: Set the playback bitrate
-SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound Spotify $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
-SOUND_SPOTIFY_BITRATE=${SOUND_SPOTIFY_BITRATE:-160}
+SOUND_DEVICE_NAME="${SOUND_DEVICE_NAME:-balenaSound Spotify ${BALENA_DEVICE_UUID:0:4}}"
+SOUND_SPOTIFY_BITRATE="${SOUND_SPOTIFY_BITRATE:-160}"
 
-# SOUND_SPOTIFY_DISABLE_NORMALISATION: Disable volume normalization
-if [[ -z ${SOUND_SPOTIFY_DISABLE_NORMALISATION+x} ]]; then
-  set -- "$@" --enable-volume-normalisation
+ARGS=(
+  --name "$SOUND_DEVICE_NAME"
+  --backend pulseaudio
+  --bitrate "$SOUND_SPOTIFY_BITRATE"
+  --cache /var/cache/raspotify
+  --volume-ctrl linear
+)
+
+if [[ -z "$SOUND_SPOTIFY_DISABLE_NORMALISATION" ]]; then
+  ARGS+=(--enable-volume-normalisation)
+else
+  echo "Volume normalization disabled."
 fi
 
-# SOUND_SPOTIFY_ENABLE_CACHE: Enable Spotify audio cache
-if [[ -z ${SOUND_SPOTIFY_ENABLE_CACHE+x} ]]; then
-  set -- "$@" --disable-audio-cache
+if [[ -z "$SOUND_SPOTIFY_ENABLE_CACHE" ]]; then
+  ARGS+=(--disable-audio-cache)
+else
+  echo "Spotify audio cache enabled."
 fi
-
-# Explicitly enable discovery mode (required for librespot >= 0.6)
-set -- "$@" --enable-discovery
 
 echo "Starting Spotify plugin..."
 echo "Device name: $SOUND_DEVICE_NAME"
-[[ -n ${SOUND_SPOTIFY_DISABLE_NORMALISATION+x} ]] && echo "Volume normalization disabled."
-[[ -n ${SOUND_SPOTIFY_ENABLE_CACHE+x} ]] && echo "Spotify audio cache enabled."
 
-# Run librespot with pulseaudio backend and passed arguments
-exec /usr/bin/librespot \
-  --backend pulseaudio \
-  --name "$SOUND_DEVICE_NAME" \
-  --bitrate "$SOUND_SPOTIFY_BITRATE" \
-  --cache /var/cache/raspotify \
-  --volume-ctrl linear \
-  "$@"
+exec /usr/bin/librespot "${ARGS[@]}"
