30d6bd31fd82823d0a6597e56daccb37c5507ee4
dinpin <dinpin@gmail.com>
2025-08-07 18:16:14 +0200

    start

--- plugins/spotify/start.sh
+++ plugins/spotify/start.sh
@@ -5,7 +5,7 @@ if [[ -n "$SOUND_DISABLE_SPOTIFY" ]]; then
   exit 0
 fi
 
-# --- ENV VARS ---
+# --- ENV VARS ---
 # SOUND_DEVICE_NAME: Set the device broadcast name for Spotify
 # SOUND_SPOTIFY_BITRATE: Set the playback bitrate
 SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound Spotify $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
@@ -13,38 +13,27 @@ SOUND_SPOTIFY_BITRATE=${SOUND_SPOTIFY_BITRATE:-160}
 
 # SOUND_SPOTIFY_DISABLE_NORMALISATION: Disable volume normalization
 if [[ -z ${SOUND_SPOTIFY_DISABLE_NORMALISATION+x} ]]; then
-  set -- "$@" \
-    --enable-volume-normalisation
-fi
-
-# SOUND_SPOTIFY_USERNAME: Login username for Spotify
-# SOUND_SPOTIFY_PASSWORD: Login password for Spotify
-if [[ -n "$SOUND_SPOTIFY_USERNAME" ]] && [[ -n "$SOUND_SPOTIFY_PASSWORD" ]]; then
-  set -- "$@" \
-    --username "$SOUND_SPOTIFY_USERNAME" \
-    --password "$SOUND_SPOTIFY_PASSWORD"
+  set -- "$@" --enable-volume-normalisation
 fi
 
 # SOUND_SPOTIFY_ENABLE_CACHE: Enable Spotify audio cache
 if [[ -z ${SOUND_SPOTIFY_ENABLE_CACHE+x} ]]; then
-  set -- "$@" \
-    --disable-audio-cache
+  set -- "$@" --disable-audio-cache
 fi
 
-# Start librespot
-# We use set/$@ because librespot for some reason does not like env vars and quote escapes
+# Explicitly enable discovery mode (required for librespot >= 0.6)
+set -- "$@" --enable-discovery
+
 echo "Starting Spotify plugin..."
 echo "Device name: $SOUND_DEVICE_NAME"
-[[ -n "$SOUND_SPOTIFY_USERNAME" ]] && [[ -n "$SOUND_SPOTIFY_PASSWORD" ]] && echo "Using provided credentials for Spotify login."
 [[ -n ${SOUND_SPOTIFY_DISABLE_NORMALISATION+x} ]] && echo "Volume normalization disabled."
 [[ -n ${SOUND_SPOTIFY_ENABLE_CACHE+x} ]] && echo "Spotify audio cache enabled."
 
-set -- /usr/bin/librespot \
+# Run librespot with pulseaudio backend and passed arguments
+exec /usr/bin/librespot \
   --backend pulseaudio \
   --name "$SOUND_DEVICE_NAME" \
   --bitrate "$SOUND_SPOTIFY_BITRATE" \
   --cache /var/cache/raspotify \
   --volume-ctrl linear \
   "$@"
-
-exec "$@"
