c12cd27f6f013a138bf1bad7295523252c862158
dinpin <dinpin@gmail.com>
2025-08-24 15:53:51 +0200

    Add enhanced AirPlay configuration with disable check and device name initialization

--- plugins/airplay/start.sh
+++ plugins/airplay/start.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+if [ -n "$SOUND_DISABLE_AIRPLAY" ]; then
+  echo "Airplay is disabled, exiting..."
+  exit 0
+fi
+
+# --- ENV VARS ---
+# SOUND_DEVICE_NAME: Set the device broadcast name for AirPlay
+SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound AirPlay $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
+
 echo "Starting AirPlay plugin..."
 echo "Device name: ${SOUND_DEVICE_NAME:-${DEVICE_NAME:-$BALENA_DEVICE_NAME_AT_INIT}}"
 
